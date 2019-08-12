1bb58664454bcf077c85c5b52f30f5370b7579cd
Tierney Cyren <hello@bnb.im>
2019-08-12 10:05:00 -0700

    Remove unnecessary stuff

--- examples/hyper.js
+++
@@ -1,9 +0,0 @@
-const gfi = require('..')
-
-gfi('zeit/hyper') // libgfi this returns a promise
-  .then((issue) => { // so we can use .then() to handle the results
-    console.log(issue)
-  })
-  .catch((error) => { // we want to be sure to catch if there are errors
-    console.log(error)
-  })
